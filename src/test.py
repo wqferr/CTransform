bd484c2d42e8c68ec062fb811ac2bf2b5a17b865
William Quelho Ferreira <wqferr>
2017-07-31 02:09:21 -0300

    Allow for different dimensions and intervals for output

--- src/test.py
+++ src/test.py
@@ -3,9 +3,16 @@ from transform import apply_to
 import sys
 
 if __name__ == '__main__':
+    img = Image.open(f'img/{sys.argv[1]}.png')
+    if len(sys.argv) > 2:
+        scale = float(sys.argv[2])
+        img = img.resize(
+            (int(scale * img.width), int(scale * img.height)),
+            Image.NEAREST
+        )
     apply_to(
-        lambda z: z * z * z,
-        Image.open(f'img/{sys.argv[1]}.png'),
-        scale=6,
-        lim=(-2, 2)
+        lambda z: z**5,
+        img,
+        lim_out=(-0.1, 0.1),
+        dim_out=(1000, 1000)
     ).show()
--- src/transform.py
+++ src/transform.py
@@ -3,37 +3,49 @@ from functools import partial, reduce
 import numpy as np
 
 
-def _apply_func_to_img(img, w, h, xlim, ylim, pixels, cur, nxt):
+def _apply_func_to_img(img, dim_in, dim_out, xlim, ylim, pixels, cur, nxt):
     pos, z = cur
-    r0 = pos % w
-    c0 = (pos - r0) / w
-    r1 = round(h * (np.imag(z) - ylim[0]) / (ylim[1] - ylim[0]))
-    c1 = round(w * (np.real(z) - xlim[0]) / (xlim[1] - xlim[0]))
-    if 0 <= c1 < w and 0 <= r1 < h:
+    r0 = pos % dim_in[0]
+    c0 = (pos - r0) / dim_in[0]
+    r1 = round(dim_out[1] * (np.imag(z) - ylim[0]) / (ylim[1] - ylim[0]))
+    c1 = round(dim_out[0] * (np.real(z) - xlim[0]) / (xlim[1] - xlim[0]))
+    if 0 <= c1 < dim_out[0] and 0 <= r1 < dim_out[1]:
         p = img.getpixel((c0, r0))
         if np.sum(p) > 0:
             pixels[c1, r1] = p
     return nxt
 
 
-def apply_to(func, img, *, xlim=(-1, 1),  ylim=(-1, 1), lim=None, scale=None):
-    if lim is not None:
-        xlim = lim
-        ylim = lim
+def apply_to(func, img, *,
+             xlim_in=(-1, 1), xlim_out=(-1, 1),
+             ylim_in=(-1, 1), ylim_out=(-1, 1),
+             lim_in=None, lim_out=None,
+             dim_out=None):
+    if lim_in is not None:
+        xlim_in = lim_in
+        ylim_in = lim_in
 
-    if scale is not None:
-        img = img.resize((scale * img.width, scale * img.height), Image.NEAREST)
+    if lim_out is not None:
+        xlim_out = lim_out
+        ylim_out = lim_out
 
-    w, h = img.size
-    newImg = Image.new(img.mode, (w, h))
+    if dim_out is None:
+        dim_out = img.size
+
+    dim_in = img.size
+    newImg = Image.new(img.mode, dim_out)
     pixels = newImg.load()
 
-    x = np.linspace(xlim[0], xlim[1], w)
-    y = np.linspace(ylim[0], ylim[1], h) * 1j
-    z0 = np.sum(np.transpose([np.tile(x, h), np.repeat(y, w)]), axis=1)
+    x = np.linspace(xlim_in[0], xlim_in[1], dim_in[0])
+    y = np.linspace(ylim_in[0], ylim_in[1], dim_in[1]) * 1j
+    z0 = np.sum(
+        np.transpose([np.tile(x, dim_in[1]), np.repeat(y, dim_in[0])]),
+        axis=1
+    )
     z1 = func(z0)
     reduce(
-        partial(_apply_func_to_img, img, w, h, xlim, ylim, pixels),
+        partial(_apply_func_to_img, img, dim_in,
+                dim_out, xlim_out, ylim_out, pixels),
         enumerate(z1)
     )
 
